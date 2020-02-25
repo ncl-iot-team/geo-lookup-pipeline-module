0b3d6962e2ee563253ff4a2a49bca26396b76f92
Nipun Balan Thekkummal <nipunbalan@gmail.com>
2020-02-25 08:47:50 +0000

    Fix JSON parse errors

--- geo-lookup.py
+++ geo-lookup.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 from kafka import KafkaConsumer, KafkaProducer
 import json
 import os
-import requests
-import traceback
-import spacy
 import LEWSJsonUtil as util
+import spacy
+import time
+import requests
 
 #--------------- Template Code, Avoid changing anything in this section --------------------------# 
 class AbstractKafkaInStreamProcessor(ABC):
@@ -27,15 +27,22 @@ class AbstractKafkaInStreamProcessor(ABC):
     def kafka_in_stream_processor(self) -> None:
 
         for message in self.consumer:
+            
+           # try:
+                self.processed_record = self.process_data(message)
+                self.produce_data_kafka(self.processed_record)
+           # except Exception as e:
+           #     print("Skipping Record..",str(e))
+
         
-            self.processed_record = self.process_data(message)
-        
-            self.produce_data_kafka(self.processed_record)
+            
 
 
 
 
-    def __init__(self,processor_name,source_topic,target_topic):
+    def __init__(self,processor_name,source_topic,target_topic,nlp_object):
+
+        self.nlp_object = nlp_object 
 
         self.processor_name = processor_name
         
@@ -43,19 +50,16 @@ class AbstractKafkaInStreamProcessor(ABC):
         
         self.target_topic = target_topic
         
-        self.bootstrap_servers = os.getenv('KAFKA_BROKER','host.docker.internal:9092')
-        #self.bootstrap_servers = 'localhost:9092'
+        #self.bootstrap_servers = os.getenv('KAFKA_BROKER','host.docker.internal:9092')
+        self.bootstrap_servers = 'localhost:9092'
         
         print("Initializing Kafka In-Stream Processor Module")
         
-        self.consumer = KafkaConsumer(source_topic,group_id = self.processor_name,
-                                      bootstrap_servers = self.bootstrap_servers,
-                                      value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+        self.consumer = KafkaConsumer(source_topic,group_id = self.processor_name, bootstrap_servers = self.bootstrap_servers,value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
        # self.producer = KafkaProducer(bootstrap_servers=self.bootstrap_servers)
 
-        self.producer = KafkaProducer(bootstrap_servers=self.bootstrap_servers,
-                                      value_serializer = lambda v: json.dumps(v).encode('utf-8'))
+        self.producer = KafkaProducer(bootstrap_servers=self.bootstrap_servers, value_serializer = lambda v: json.dumps(v).encode('utf-8'))
 
 
 
@@ -74,21 +78,21 @@ def run(abstract_class: AbstractKafkaInStreamProcessor) -> None:
 
 #-------------------------Template Code Ends Here ------------------------------------#
 
-class ConKafkaInStreamProcessor(AbstractKafkaInStreamProcessor):
-    
-    def __init__(self,processor_name,source_topic,target_topic):
-        super(ConKafkaInStreamProcessor,self).__init__(processor_name,source_topic,target_topic)
+#------------------------- Geo Lookup Main Code --------------------------------------#
+class NlpResearch:
+    def __init__(self):
+        self.chunksize = 1000
         self.output_fields = 'text'
-        self.gcache = dict()   
-        self.nlp = spacy.load("en_core_web_lg")
-        self.osm_lookup_url = os.getenv('OSM_LOOKUP_URL','http://localhost:2322/api/?q=%s')
+        self.gcache = dict()
+        self.url_template = "https://photon.komoot.de/api/?q=%s"
+        # self.url_template = "http://localhost:2322/api/?q=%s"
+        self.nlp = spacy.load("en_core_web_sm")
 
-    
-    
+    # ----Spacy Functions --------------
     # Helps in converting the first letter of each sentence to capital
     def preprocess(self, text):
         return text.title()
-            
+
     # Function which helps in fetching the geotag from the source input file
     def get_geotag(self, text):
         gtag = []
@@ -104,12 +108,12 @@ class ConKafkaInStreamProcessor(AbstractKafkaInStreamProcessor):
         return gtag
 
     # Function to run the whole process first capitalising the first letter than finding geotags from input
-            
     def process(self, text):
         ptext = self.preprocess(text)
-        return self.get_geotag(ptext)       
-        
-                
+        return self.get_geotag(ptext)
+
+    # -------------------------------------------------------------
+
     # Function which saves converted geo-coordinates in cache so the program should not contact OSM server for getting
     # information of repeated geotags
     def geo_cache(self, place):
@@ -119,100 +123,114 @@ class ConKafkaInStreamProcessor(AbstractKafkaInStreamProcessor):
         tags = self.osm_coordinates(place)
         self.gcache[place] = tags
         return tags
-            
-    
-    
+
     # Connects to the service and check whether return value is city, country, state and town
     # from the input text which are tweets to fetch latitudes and longitudes
     def osm_coordinates(self, place):
-        not_found = (None, None)
-        req = requests.get(self.osm_lookup_url % place)
+        out = []
+        response = requests.get(self.url_template % place)
         try:
-            data = req.json()
+            data = response.json()
             for d in data['features']:
-                if 'place' != d['properties']['osm_key']:
-                    continue
-                if not d['properties']['osm_value'] in ['country', 'city', 'county', 'village', 'town']:
-                    continue
+                name = d['properties']['name']
                 lon = d['geometry']['coordinates'][0]
                 lat = d['geometry']['coordinates'][1]
-                return (lat, lon)
-        except:
+                out.append((name, lon, lat))
+    
+        except Exception as err:
             ## errors when spacy return wrong names for places which has unsupported characters like '#Endomondo'
-            return not_found
-        return not_found
-    
-    
-    
+            print(err)
+        return out
+
     # Each input line calls process function runs spacy to fetch geotags then convert them
-    #into geo-coordinates and store it in mentions list and create require structure
-    def parse_pandas(self,text,time):
-        mentions = []
+    # into geo-coordinates and store it in mentions list and create require structure
+    def parse_input(self, data):
+        text = data['text']
+        timestamp = data['timestamp_ms']
         plist = self.process(text)
-        mentions.append(self.places_to_geo_coordinates(plist))
+        mentions = self.places_to_geo_coordinates(plist)
 
         try:
-            result = self.create_output_struct(text,time,mentions)
+            result = self.create_output_struct(text, timestamp, mentions)
         except Exception as err:
             print(err)
             print(traceback.print_exc())
             return []
 
-        return(result)    
-       
-     # Extracts all geo-places checks for cache if converted lat and long is present for that particular
-    #geotag if present accesses it else connect to service to get geo-coordinates
+        return (result)
+
+    # Extracts all geo-places checks for cache if converted lat and long is present for that particular
+    # geotag if present accesses it else connect to service to get geo-coordinates
 
     def places_to_geo_coordinates(self, places):
         gtags = []
         for p in places:
             if len(p) > 0:
                 tags = self.geo_cache(p)
-                if not tags[0] is None: # filter out things which spacy incorrectly detected  as a place
-                    gtags.append((p, tags[0], tags[1]))
+                if 0 < len(tags):  # filter out things which spacy incorrectly detected  as a place
+                    gtags.append((p, tags))
         return gtags
-            
-    #To create a JSON format output strcture which makes it easy
-            #for indexing in elastic search
-    def create_output_struct(self, text,time, user_mentions):
-        
-       # result = {'media':'twitter'}
-       # result['location']= {'geotag':{'lat': tab['y'], 'lon': tab['x']}}
-        mentions = []
-        results = []
-        cur_mention = user_mentions[0]
-        #print(user_mentions)
-        #print(cur_mention)
-        for m in cur_mention:
-            mentions.append({'place': m[0], 'geotag':{'lat': m[1], 'lon': m[2]}})
-        result[0] = mentions
-        result[1] = time
-        result[2] = 'twitter'
-        #result['time of publish'] = time
-        #result['raw_data'] = text
-                
-        return result
-       
 
-    def process_data(self,message) -> None:
-        
-        #print(message.value)        
+    # To create a JSON format output strcture which makes it easy
+    # for indexing in elastic search
+    def create_output_struct(self, text, timestamp, user_mentions):
+        result = {'media': 'twitter'}
+        result['time of publish'] = timestamp
+        result['raw_data'] = text
+        mentions = []
+        for m in user_mentions:
+            for rec in m[1]:
+                mentions.append({'place': rec[0], 'geotag': {'lat': rec[1], 'lon': rec[2]}})
+        result['mentions'] = mentions
+
+        return result
+
+    # Pandas data analytics framework to access data easily
+    def process_input(self):
+        f = open('test.txt', 'r')
+        input = f.read()
+        result = self.parse_input(json.loads(input))
+        return result
+
+
+#------------------------- Geo Lookup Main Code Ends Here ----------------------------#
+
+
+class ConKafkaInStreamProcessor(AbstractKafkaInStreamProcessor):
+
+     def process_data(self,message) -> None:
+#------------------- Add module Logic in this section ---------------------#
         try:
             #-- Perform all the module logic here --#
+
             # To get value from a field (Example)
             util.json_util = util.JsonDataUtil(message.value)
-            results = self.parse_pandas(util.json_util.get_value("text"),util.json_util.get_value("created_at"))
-            util.json_util.add_metadata("media",results[2])
-            util.json_util.add_metadata("time",results[1])
-            util.json_util.add_metadata("mentions",results[0])
-        
+            
+
+            tweet_text = util.json_util.get_value("text")
+            #Do Processing
+
+            plist = self.nlp_object.process(tweet_text)
+            mentions = self.nlp_object.places_to_geo_coordinates(plist) 
+
+
+
+            #Adding metadata to the record (Example)
+            util.json_util.add_metadata("mentions",json.dumps(mentions))
+            #util.json_util.add_metadata("Longitude","-1.617780")
+
         except:
             print("Invalid Tweet Record.. Skipping")
             raise
 
-        processes_message = util.json_util.get_json() 
-                        
+        #Get the processed record with metadata added
+        processes_message = util.json_util.get_json()
+        #print("Processes Message:",json.dumps(processes_message))
+        #time.sleep(1)
+#---------------------- Add module logic in this section (End) ----------------------#
         return processes_message
+
+
 
 
 
@@ -224,6 +242,7 @@ if __name__ == "__main__":
    s_topic = os.getenv('MODULE_SRC_TOPIC','lews-twitter')
    t_topic = os.getenv('MODULE_TGT_TOPIC','t_topic')
    proc_name = os.getenv('MODULE_NAME','Module01')
+
+   nlp_obj = NlpResearch()
    
-   
-   run(ConKafkaInStreamProcessor(processor_name=proc_name, source_topic=s_topic, target_topic=t_topic))
+   run(ConKafkaInStreamProcessor(processor_name=proc_name, source_topic=s_topic, target_topic=t_topic, nlp_object=nlp_obj))
